958f725fcd705737c4d2fda7123eac053113e43b
Bassim saeed <182443885+bassemsaeed@users.noreply.github.com>
2026-02-28 01:10:28 +0200

    adds print from n to 1 problem solution

---
+++ C_Print_from_N_to_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void print_n_to_1(int n) {
+  if (n == 0)
+    return;
+
+  cout << n << "\n";   // Print first
+  print_n_to_1(n - 1); // Recurse after
+}
+
+int main() {
+  int n;
+  cin >> n;
+  print_n_to_1(n);
+  return 0;
+}
